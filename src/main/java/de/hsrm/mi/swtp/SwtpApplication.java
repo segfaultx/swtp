523e0b2ef9d8fbad40cf0da3853a96735f98927e
Dennis Schad <dschad@comwrap.com>
2019-11-13 09:35:54 +0100

    Cleanup Working build again

--- src/main/java/de/hsrm/mi/swtp/SwtpApplication.java
+++ src/main/java/de/hsrm/mi/swtp/SwtpApplication.java
@@ -1,13 +1,8 @@
 package de.hsrm.mi.swtp;
 
-import de.hsrm.mi.swtp.exchangeplatform.configuration.AppConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
-@Import({
-        AppConfig.class
-})
 @SpringBootApplication
 public class SwtpApplication {
 
