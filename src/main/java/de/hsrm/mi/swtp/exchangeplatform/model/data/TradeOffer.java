7fb15c542cbaf805ed4f1d013c701ceabcdc8c9b
Amatus Muscheites <amatus.muscheites@gmail.com>
2019-11-26 14:10:35 +0100

    TradeOffer entity implemented

---
+++ src/main/java/de/hsrm/mi/swtp/exchangeplatform/model/data/TradeOffer.java
@@ -0,0 +1,32 @@
+package de.hsrm.mi.swtp.exchangeplatform.model.data;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class TradeOffer implements Model {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    private Student offerer;
+
+    @ManyToOne
+    private Timeslot offer;
+
+    @ManyToOne
+    private Timeslot seek;
+
+    private boolean instantTrade = false;
+}
