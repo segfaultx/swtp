52ce1ba1dc3e6a5e8efb035065fbf9f95cba83b1
Henock Arega <henock.a@hotmail.de>
2019-12-04 10:44:11 +0100

    Reformatted code with .editorconfig settings after merge.

--- src/main/java/de/hsrm/mi/swtp/exchangeplatform/model/data/Student.java
+++ src/main/java/de/hsrm/mi/swtp/exchangeplatform/model/data/Student.java
@@ -1,35 +1,36 @@
 package de.hsrm.mi.swtp.exchangeplatform.model.data;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import java.util.List;
 
 @Entity
 @Data
 public class Student implements Model {
-
-    @Id
-    @JsonProperty(value = "matriculationNumber", required = true)
-    @Column(name = "matr_nr", unique = true, updatable = false, insertable = false)
-    private Long matriculationNumber;
-
-    @JsonProperty("username")
-    @Column(unique = true)
-    private String username;
-
-    @JsonProperty("first_name")
-    private String firstName;
-
-    @JsonProperty("last_name")
-    private String lastName;
-
-    @JsonManagedReference
-    @ManyToMany(mappedBy = "attendees")
-    private List<Timeslot> timeslots;
-
+	
+	@Id
+	@JsonProperty(value = "matriculationNumber", required = true)
+	@Column(name = "matr_nr", unique = true, updatable = false, insertable = false)
+	private Long matriculationNumber;
+	
+	@JsonProperty("username")
+	@Column(unique = true)
+	private String username;
+	
+	@JsonProperty("first_name")
+	private String firstName;
+	
+	@JsonProperty("last_name")
+	private String lastName;
+	
+	@JsonManagedReference
+	@ManyToMany(mappedBy = "attendees")
+	private List<Timeslot> timeslots;
+	
 }
--- src/main/java/de/hsrm/mi/swtp/exchangeplatform/model/data/Timeslot.java
+++ src/main/java/de/hsrm/mi/swtp/exchangeplatform/model/data/Timeslot.java
@@ -2,7 +2,6 @@ package de.hsrm.mi.swtp.exchangeplatform.model.data;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
@@ -20,90 +19,91 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 @Table(name = "timeslot")
 public class Timeslot implements Model {
-
-    /** A unique identifier of an appointment by which it can be found. */
-    @Id
-    @GeneratedValue
-    private Long id;
-
-    /** The room in which the appointment will take place. */
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    private Room room;
-
-    /** The day on which the appointment is scheduled for. */
-    private String day;
-
-    /** The scheduled beginning of the appointment. */
-    @JsonProperty("time_start")
-    @JsonSerialize(using = LocalTimeSerializer.class)
-    private LocalTime timeStart;  // string nur weil aktuell in lokalen DB ein falscher Eintrag war
-
-    /** The scheduled end time of the appointment. */
-    @JsonProperty("time_end")
-    @JsonSerialize(using = LocalTimeSerializer.class)
-    private LocalTime timeEnd;  // string nur weil aktuell in lokalen DB ein falscher Eintrag war
-
-    /** The lecturer of the appointment. */
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    private Lecturer lecturer;
-
-    /**
-     * The type of the appointment. Is of type {@link}.
-     */
-    private String type;
-
-    /** The maximum amount of {@link Student Students} which can join an appointment. */
-    private int capacity;
-
-    /** A list of {@link Student Students} which have joined an appointment. */
-    @JsonBackReference
-    @ManyToMany(cascade = CascadeType.ALL)
-    private List<Student> attendees;
-
-    @JsonIgnore
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    @JoinColumn(name = "module_id")
-    private Module module;
-
-    @JsonIgnore
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    private TimeTable timeTable;
-
-    /**
-     * A small filter which searches {@link #attendees}.
-     *
-     * @return a {@link Student} if found in {@link #attendees}. Otherwise will return null.
-     */
-    private Student filterAttendeesForStudent(Student student) {
-        List<Student> students = this.attendees
-                .stream()
-                .filter(student1 -> student1.getMatriculationNumber().equals(student.getMatriculationNumber()))
-                .collect(Collectors.toList());
-        return students.isEmpty() ? null : students.get(0);
-    }
-
-    /**
-     * Adds a student to the list of {@link #attendees} if student is not an attendee already.
-     *
-     * @param student the student which is to be added to the list of {@link #attendees}.
-     */
-    public boolean addAttendee(Student student) {
-        return this.filterAttendeesForStudent(student) == null && this.attendees.add(student);
-    }
-
-    /**
-     * Removes a student from the list of {@link #attendees} of an appointment.
-     *
-     * @param student the {@link Student} which leaves an appointment.
-     * @return a boolean which indicated whether the student was removed successfully.
-     * @throws ModelNotFoundException if the given student was not found in the list of {@link #attendees}.
-     */
-    public boolean removeAttendee(Student student) {
-        Student foundStudent = this.filterAttendeesForStudent(student);
-        if (foundStudent == null) {
-            throw new ModelNotFoundException(student);
-        }
-        return this.attendees.remove(student);
-    }
-
+	
+	/** A unique identifier of an appointment by which it can be found. */
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	/** The room in which the appointment will take place. */
+	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	private Room room;
+	
+	/** The day on which the appointment is scheduled for. */
+	private String day;
+	
+	/** The scheduled beginning of the appointment. */
+	@JsonProperty("time_start")
+	@JsonSerialize(using = LocalTimeSerializer.class)
+	private LocalTime timeStart;  // string nur weil aktuell in lokalen DB ein falscher Eintrag war
+	
+	/** The scheduled end time of the appointment. */
+	@JsonProperty("time_end")
+	@JsonSerialize(using = LocalTimeSerializer.class)
+	private LocalTime timeEnd;  // string nur weil aktuell in lokalen DB ein falscher Eintrag war
+	
+	/** The lecturer of the appointment. */
+	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	private Lecturer lecturer;
+	
+	/**
+	 * The type of the appointment. Is of type {@link}.
+	 */
+	private String type;
+	
+	/** The maximum amount of {@link Student Students} which can join an appointment. */
+	private int capacity;
+	
+	/** A list of {@link Student Students} which have joined an appointment. */
+	@JsonBackReference
+	@ManyToMany(cascade = CascadeType.ALL)
+	private List<Student> attendees;
+	
+	@JsonIgnore
+	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "module_id")
+	private Module module;
+	
+	@JsonIgnore
+	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	private TimeTable timeTable;
+	
+	/**
+	 * Adds a student to the list of {@link #attendees} if student is not an attendee already.
+	 *
+	 * @param student the student which is to be added to the list of {@link #attendees}.
+	 */
+	public boolean addAttendee(Student student) {
+		return this.filterAttendeesForStudent(student) == null && this.attendees.add(student);
+	}
+	
+	/**
+	 * A small filter which searches {@link #attendees}.
+	 *
+	 * @return a {@link Student} if found in {@link #attendees}. Otherwise will return null.
+	 */
+	private Student filterAttendeesForStudent(Student student) {
+		List<Student> students = this.attendees.stream()
+											   .filter(student1 -> student1.getMatriculationNumber().equals(student.getMatriculationNumber()))
+											   .collect(Collectors.toList());
+		return students.isEmpty() ? null : students.get(0);
+	}
+	
+	/**
+	 * Removes a student from the list of {@link #attendees} of an appointment.
+	 *
+	 * @param student the {@link Student} which leaves an appointment.
+	 *
+	 * @return a boolean which indicated whether the student was removed successfully.
+	 *
+	 * @throws ModelNotFoundException if the given student was not found in the list of {@link #attendees}.
+	 */
+	public boolean removeAttendee(Student student) {
+		Student foundStudent = this.filterAttendeesForStudent(student);
+		if(foundStudent == null) {
+			throw new ModelNotFoundException(student);
+		}
+		return this.attendees.remove(student);
+	}
+	
 }
