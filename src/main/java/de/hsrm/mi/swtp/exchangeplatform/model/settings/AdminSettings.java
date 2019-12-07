726625384fa4009154cd2ca3a30343f6aa31453e
Amatus Muscheites <amatus.muscheites@gmail.com>
2019-12-07 12:24:06 +0100

    added adminSettings class

---
+++ src/main/java/de/hsrm/mi/swtp/exchangeplatform/model/settings/AdminSettings.java
@@ -0,0 +1,39 @@
+package de.hsrm.mi.swtp.exchangeplatform.model.settings;
+
+import de.hsrm.mi.swtp.exchangeplatform.service.filter.*;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Data
+@Entity
+public class AdminSettings {
+	
+	@Id
+	@GeneratedValue
+	long id;
+	
+	boolean tradesActive = true;
+	
+	@Getter
+	enum Filters {
+		COLLISION(new CollisionFilter()), CAPACITY(new CapacityFilter()), OFFER(new OfferFilter()), NOOFFER(new NoOfferFilter());
+		
+		Filters(Filter filter) {
+			this.filter = filter;
+		}
+		
+		private Filter filter;
+	}
+	
+	@ElementCollection
+	@Enumerated(EnumType.STRING)
+	List<Filters> activeFilters;
+}
