1dab7eb16dff5ff173ac8de0676338c061eb697c
Amatus Muscheites <amatus.muscheites@gmail.com>
2019-12-07 12:09:47 +0100

    removed unused import

--- src/main/java/de/hsrm/mi/swtp/exchangeplatform/service/rest/TradeOfferService.java
+++ src/main/java/de/hsrm/mi/swtp/exchangeplatform/service/rest/TradeOfferService.java
@@ -14,7 +14,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
