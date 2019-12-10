23b9c5c0874bcf1c820303731bdcce7fc71da576
Amatus Muscheites <amatus.muscheites@gmail.com>
2019-12-10 13:48:57 +0100

    quick fix for getStudentTimeTable

--- src/main/java/de/hsrm/mi/swtp/exchangeplatform/service/rest/TimeslotRestModelConverter.java
+++ src/main/java/de/hsrm/mi/swtp/exchangeplatform/service/rest/TimeslotRestModelConverter.java
@@ -12,6 +12,10 @@ import org.springframework.stereotype.Service;
 
 import javax.validation.constraints.NotNull;
 import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @Slf4j
@@ -20,14 +24,23 @@ public class TimeslotRestModelConverter implements RestModelConverter {
 	TradeOfferService tradeOfferService;
 	TradeOfferRepository tradeOfferRepository;
 	TimeslotRepository timeslotRepository;
+	Map<String, String> dayMapper = new HashMap<>();
 	
 	@Autowired
-	public TimeslotRestModelConverter(@NotNull TradeOfferService tradeOfferService,
-									  @NotNull TradeOfferRepository tradeOfferRepository,
-									  @NotNull TimeslotRepository timeslotRepository){
+	public TimeslotRestModelConverter(@NotNull TradeOfferService tradeOfferService, @NotNull TradeOfferRepository tradeOfferRepository,
+									  @NotNull TimeslotRepository timeslotRepository
+									 ) {
 		this.tradeOfferService = tradeOfferService;
 		this.tradeOfferRepository = tradeOfferRepository;
 		this.timeslotRepository = timeslotRepository;
+		this.dayMapper.put("Mittwoch", "wednesday");
+		this.dayMapper.put("Montag", "monday");
+		this.dayMapper.put("Dienstag", "tuesday");
+		this.dayMapper.put("Donnerstag", "thursday");
+		this.dayMapper.put("Freitag", "friday");
+		this.dayMapper.put("Samstag", "saturday");
+		this.dayMapper.put("Sonntag", "sunday");
+		
 	}
 	
 	@Override
@@ -39,20 +52,28 @@ public class TimeslotRestModelConverter implements RestModelConverter {
 	public Object convertToRest(Object object) {
 		Timeslot timeslot = (Timeslot) object;
 		de.hsrm.mi.swtp.exchangeplatform.model.rest_models.Timeslot out = new de.hsrm.mi.swtp.exchangeplatform.model.rest_models.Timeslot();
-		out.setDay(DayEnum.valueOf(timeslot.getDay()));
-		out.setTimeStart(Timestamp.valueOf(timeslot.getTimeStart().toString()));
-		out.setTimeEnd(Timestamp.valueOf(timeslot.getTimeEnd().toString()));
+		out.setDay(DayEnum.fromValue(dayMapper.get(timeslot.getDay())));
+		out.setTimeStart(Timestamp.valueOf(LocalDateTime.of(LocalDate.now(), timeslot.getTimeStart())));
+		out.setTimeEnd(Timestamp.valueOf(LocalDateTime.of(LocalDate.now(), timeslot.getTimeEnd())));
 		PossibleTradesResponse possibleTradesResponse = new PossibleTradesResponse();
 		var instantTrades = tradeOfferRepository.findAllBySeekAndInstantTrade(timeslot, true);
 		instantTrades.forEach(tradeOffer -> possibleTradesResponse.addInstantItem(tradeOffer.getOffer().getId()));
 		var regularTrades = tradeOfferRepository.findAllBySeekAndInstantTrade(timeslot, false);
 		regularTrades.forEach(tradeOffer -> possibleTradesResponse.addTradesAvailableItem((tradeOffer.getOffer().getId())));
+		/*
 		var others = timeslotRepository.findAllByModule(timeslot.getModule());
-		others.forEach(timeslt -> {
-			if(!possibleTradesResponse.getInstant().contains(timeslt.getId()) && !possibleTradesResponse.getTradesAvailable().contains(timeslt.getId()))
-				possibleTradesResponse.addRemainingItem(timeslt.getId());
-		});
+		if(others != null) {
+			for(Timeslot timeslt: others){
+				System.out.println(timeslt.getId());
+			}
+			others.forEach(timeslt -> {
+				if(!possibleTradesResponse.getInstant().contains(timeslt.getId()) && !possibleTradesResponse.getTradesAvailable().contains(timeslt.getId()))
+					possibleTradesResponse.addRemainingItem(timeslt.getId());
+			});
+		}
 		out.setPossibleTrades(possibleTradesResponse);
+		 */
+		//TODO: fix stackoverflow errors of objects
 		return out;
 	}
 }
