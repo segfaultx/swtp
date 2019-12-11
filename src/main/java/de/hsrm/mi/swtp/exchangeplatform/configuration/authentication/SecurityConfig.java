a9538949172b273d564fad981fa89569f4aa512f
Henock Arega <harega@seibert-media.net>
2019-12-11 12:24:12 +0100

    Revert "Fixed URL-paths for authentication and re-added /exchangeplatform as base path."
    This reverts commit 9455ecde

--- src/main/java/de/hsrm/mi/swtp/exchangeplatform/configuration/authentication/SecurityConfig.java
+++ src/main/java/de/hsrm/mi/swtp/exchangeplatform/configuration/authentication/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 // We don't need CSRF for this example
 		httpSecurity.csrf().disable()
 // dont authenticate this particular request
-					.authorizeRequests().antMatchers("/api/v1/auth/login").permitAll().
+					.authorizeRequests().antMatchers("/exchangeplatform/api/v1/auth/login").permitAll().
 // all other requests need to be authenticated
 		anyRequest().authenticated().and().
 // make sure we use stateless session; session won't be used to
