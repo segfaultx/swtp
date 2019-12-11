b3635a48c0779ddbd4936ad4fa99637d2709304c
Segfault <amatus.muscheites@gmail.com>
2019-12-11 12:12:16 +0100

    added @JsonIgnoreProperties annotation to model interface

--- src/main/java/de/hsrm/mi/swtp/exchangeplatform/model/data/Model.java
+++ src/main/java/de/hsrm/mi/swtp/exchangeplatform/model/data/Model.java
@@ -1,5 +1,8 @@
 package de.hsrm.mi.swtp.exchangeplatform.model.data;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.io.Serializable;
 
+@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler"})
 public interface Model extends Serializable {}
