cfc6c6754f00c5cf216f063010736aee2321be33
Amatus Muscheites <amatus.muscheites@gmail.com>
2019-12-11 11:52:06 +0100

    removed basrestcontroller interface from Timetablerestcontroller, fixed api annotations

--- src/main/java/de/hsrm/mi/swtp/exchangeplatform/controller/TimeTableController.java
+++ src/main/java/de/hsrm/mi/swtp/exchangeplatform/controller/TimeTableController.java
@@ -6,7 +6,7 @@ import de.hsrm.mi.swtp.exchangeplatform.model.rest_models.TimetableDTO;
 import de.hsrm.mi.swtp.exchangeplatform.service.rest.RestConverterService;
 import de.hsrm.mi.swtp.exchangeplatform.service.rest.StudentService;
 import de.hsrm.mi.swtp.exchangeplatform.service.rest.TimeTableService;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.*;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -16,70 +16,28 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @Slf4j
 @RequiredArgsConstructor
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 @RequestMapping("/api/v1/timetables")
 @RestController
-public class TimeTableController implements BaseRestController<TimeTable, Long> {
+@Api(value = "timetables", description = "the timetables API")
+public class TimeTableController {
 	
 	TimeTableService timeTableService;
 	StudentService studentService;
-	RestConverterService restConverterService;
 	
-	@Override
-	@GetMapping
-	public ResponseEntity<List<TimeTable>> getAll() {
-		return new ResponseEntity<>(timeTableService.getAll(), HttpStatus.OK);
-	}
-	
-	@Override
-	@GetMapping("/{id}")
-	public ResponseEntity<TimeTable> getById(@PathVariable Long id) {
-		try {
-			return new ResponseEntity<>(timeTableService.getById(id), HttpStatus.OK);
-		} catch(NotFoundException e) {
-			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-		}
-	}
-	
-	@Override
-	@PostMapping
-	public ResponseEntity<TimeTable> create(@RequestBody TimeTable timeTable, BindingResult result) {
-		if(result.hasErrors()) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-		
-		timeTableService.save(timeTable);
-		return new ResponseEntity<>(timeTable, HttpStatus.OK);
-	}
-	
-	@Override
-	public ResponseEntity<TimeTable> update(@PathVariable Long id, @RequestBody TimeTable timeTable, BindingResult result) {
-		return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
-	}
-	
-	@Override
-	public ResponseEntity<TimeTable> delete(@PathVariable Long id) {
-		try {
-			this.timeTableService.delete(id);
-			return new ResponseEntity<>(HttpStatus.OK);
-		} catch(NotFoundException e) {
-			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-		} catch(IllegalArgumentException e) {
-			log.info(String.format("FAIL: Something went wrong during deletion of timetable %s", id));
-			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-		}
-	}
-	
-	@ApiOperation(value = "Get personalized Timetable of student.", nickname = "getTimetableForStudent", notes = "", response = TimetableDTO.class, tags = { })
+	@ApiOperation(value = "Get personalized Timetable of student.", nickname = "getTimetableForStudent", notes = "", response = TimeTable.class, tags = { })
 	@RequestMapping(value = "/{studentId}", produces = { "application/json" }, method = RequestMethod.GET)
-	public ResponseEntity<TimetableDTO> getTimetableForStudent(@PathVariable(value = "studentId") Long studentId) {
+	
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "a timetable", response = TimeTable.class) })
+	public ResponseEntity<TimeTable> getTimetableForStudent(
+			@ApiParam(value = "Numeric ID of the student", required = true) @PathVariable(value = "studentId") Long studentId
+														   ) {
 		var stud = studentService.getById(studentId);
 		TimeTable timeTable = new TimeTable();
 		timeTable.setTimeslots(stud.getTimeslots());
-		TimetableDTO out = (TimetableDTO) restConverterService.convert(timeTable);
-		out.setId(stud.getStudentId());
-		return new ResponseEntity<>(out, HttpStatus.OK);
+		timeTable.setId(stud.getStudentId());
+		return new ResponseEntity<>(timeTable, HttpStatus.OK);
 	}
 }
