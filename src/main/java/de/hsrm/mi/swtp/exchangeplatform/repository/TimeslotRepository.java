697d80a792706250afbe628ea4567ecb88f82dc0
Dennis Schad <dschad@comwrap.com>
2019-12-12 13:52:04 +0100

    Fixed Issue with query param

--- src/main/java/de/hsrm/mi/swtp/exchangeplatform/repository/TimeslotRepository.java
+++ src/main/java/de/hsrm/mi/swtp/exchangeplatform/repository/TimeslotRepository.java
@@ -4,6 +4,7 @@ import de.hsrm.mi.swtp.exchangeplatform.model.data.Module;
 import de.hsrm.mi.swtp.exchangeplatform.model.data.Timeslot;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,5 +14,5 @@ public interface TimeslotRepository extends JpaRepository<Timeslot, Long> {
 	List<Timeslot> findAllByModule(Module module);
 	
 	@Query("select user.id from User user join Timeslot ts where ts.id = :id")
-	List<Timeslot> findAllAttendeesByTimeSlotId(Long id);
+	List<Timeslot> findAllAttendeesByTimeSlotId(@Param("id") Long id);
 }
