b36b621141816a9720d9f996c9d00af939813d1c
Dennis Schad <dschad@comwrap.com>
2019-12-12 12:19:09 +0100

    Refactored Repositories

--- src/main/java/de/hsrm/mi/swtp/exchangeplatform/repository/LecturerRepository.java
+++
@@ -1,8 +0,0 @@
-package de.hsrm.mi.swtp.exchangeplatform.repository;
-
-import de.hsrm.mi.swtp.exchangeplatform.model.data.Lecturer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LecturerRepository extends JpaRepository<Lecturer, Long> {}
--- src/main/java/de/hsrm/mi/swtp/exchangeplatform/repository/UserRepository.java
+++ src/main/java/de/hsrm/mi/swtp/exchangeplatform/repository/UserRepository.java
@@ -2,9 +2,12 @@ package de.hsrm.mi.swtp.exchangeplatform.repository;
 
 import de.hsrm.mi.swtp.exchangeplatform.model.data.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+	
+	@Query("select user from User user left join fetch user.authenticationInformation ai where ai.username =:username")
 	Optional<User> findByUsername(String username);
 }
