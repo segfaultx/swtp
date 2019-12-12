b1375e906b3a604121c7e0ca8ebe70b3b0c91727
Dennis Schad <dschad@comwrap.com>
2019-12-12 12:18:16 +0100

    removed unused imports

--- src/main/java/de/hsrm/mi/swtp/exchangeplatform/model/data/UserType.java
+++ src/main/java/de/hsrm/mi/swtp/exchangeplatform/model/data/UserType.java
@@ -1,17 +1,11 @@
 package de.hsrm.mi.swtp.exchangeplatform.model.data;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
-import de.hsrm.mi.swtp.exchangeplatform.model.data.enums.DayOfWeek;
-import de.hsrm.mi.swtp.exchangeplatform.model.data.enums.TypeOfTimeslots;
 import de.hsrm.mi.swtp.exchangeplatform.model.data.enums.TypeOfUsers;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
 
 import javax.persistence.*;
-import java.time.LocalTime;
 
 @Entity
 @Data
