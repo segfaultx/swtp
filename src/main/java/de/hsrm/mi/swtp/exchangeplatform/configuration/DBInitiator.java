10bdb9a12271be0dc3f8c66fba44ab1c0b185d81
Amatus Muscheites <amatus.muscheites@gmail.com>
2019-12-16 11:00:14 +0100

    added timeslots to users

--- src/main/java/de/hsrm/mi/swtp/exchangeplatform/configuration/DBInitiator.java
+++ src/main/java/de/hsrm/mi/swtp/exchangeplatform/configuration/DBInitiator.java
@@ -6,10 +6,7 @@ import de.hsrm.mi.swtp.exchangeplatform.model.data.enums.DayOfWeek;
 import de.hsrm.mi.swtp.exchangeplatform.model.data.enums.Roles;
 import de.hsrm.mi.swtp.exchangeplatform.model.data.enums.TypeOfTimeslots;
 import de.hsrm.mi.swtp.exchangeplatform.model.data.enums.TypeOfUsers;
-import de.hsrm.mi.swtp.exchangeplatform.repository.TimeTableRepository;
-import de.hsrm.mi.swtp.exchangeplatform.repository.TimeslotRepository;
-import de.hsrm.mi.swtp.exchangeplatform.repository.TradeOfferRepository;
-import de.hsrm.mi.swtp.exchangeplatform.repository.UserRepository;
+import de.hsrm.mi.swtp.exchangeplatform.repository.*;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -32,6 +29,10 @@ public class DBInitiator implements ApplicationRunner {
 	TimeslotRepository timeslotRepository;
 	
 	UserRepository userRepository;
+	
+	ModuleRepository moduleRepository;
+	
+	RoomRepository roomRepository;
 	
 	
 	@Override
@@ -94,11 +95,11 @@ public class DBInitiator implements ApplicationRunner {
 		chandler.setEmail("chandler.bing@student.hs-rm.de");
 		chandler.setStaffNumber(null);
 		
-		AuthenticationInformation williAuthInfo = new AuthenticationInformation();
-		williAuthInfo.setRole(Roles.MEMBER);
-		williAuthInfo.setUser(chandler);
-		williAuthInfo.setPassword("chandler123");
-		williAuthInfo.setUsername("cbing001");
+		AuthenticationInformation chandlerAuthInfo = new AuthenticationInformation();
+		chandlerAuthInfo.setRole(Roles.MEMBER);
+		chandlerAuthInfo.setUser(chandler);
+		chandlerAuthInfo.setPassword("chandler123");
+		chandlerAuthInfo.setUsername("cbing001");
 		
 		UserType chandlerType = new UserType();
 		chandlerType.setType(TypeOfUsers.STUDENT);
@@ -164,6 +165,8 @@ public class DBInitiator implements ApplicationRunner {
 		d12.setRoomNumber("D12");
 		d12.setTimeslots(null);
 		
+		var d12_out = roomRepository.save(d12);
+		
 		
 		// START AFS Timeslots
 		
@@ -174,13 +177,11 @@ public class DBInitiator implements ApplicationRunner {
 		afsVorlesung.setTimeStart(LocalTime.of(8, 15));
 		afsVorlesung.setTimeEnd(LocalTime.of(9, 45));
 		
-		afsVorlesung.setUser(krechel);
-		
-		afsVorlesung.setModule(afs);
-		afsVorlesung.setRoom(d12);
+		afsVorlesung.setRoom(d12_out);
 		afsVorlesung.setTimeTable(null);
 		
 		
+		// START AFS UEBUNG 1
 		Timeslot afsUebung = new Timeslot();
 		afsUebung.setTimeSlotType(TypeOfTimeslots.UEBUNG);
 		afsUebung.setCapacity(50);
@@ -188,25 +189,53 @@ public class DBInitiator implements ApplicationRunner {
 		afsUebung.setTimeStart(LocalTime.of(10, 0));
 		afsUebung.setTimeEnd(LocalTime.of(11, 30));
 		
-		afsUebung.setUser(krechel);
-		
-		afsUebung.setModule(afs);
-		afsUebung.setRoom(d12);
+		afsUebung.setRoom(d12_out);
 		afsUebung.setTimeTable(null);
+		
+		// END AFS UEBUNG 1
+		
+		// START AFS UEBUNG 2
+		
+		Timeslot afsUebung2 = new Timeslot();
+		afsUebung2.setTimeSlotType(TypeOfTimeslots.UEBUNG);
+		afsUebung2.setCapacity(50);
+		afsUebung2.setDay(DayOfWeek.WEDNESDAY);
+		afsUebung2.setTimeStart(LocalTime.of(11, 45));
+		afsUebung2.setTimeEnd(LocalTime.of(13, 15));
+		
+		afsUebung2.setRoom(d12_out);
+		afsUebung2.setTimeTable(null);
+		
+		// END AFS UEBUNG 2
+		
+		var krechel_out = userRepository.save(krechel);
+		afsUebung.setUser(krechel_out);
+		afsUebung2.setUser(krechel_out);
+		afsVorlesung.setUser(krechel_out);
+		userRepository.save(weitz);
+		// SAVE Lecturers first so attendees can reference them
+		List<Timeslot> toSave = new ArrayList<>();
+		toSave.add(afsVorlesung);
+		toSave.add(afsUebung);
+		toSave.add(afsUebung2);
+		
+		d12_out.setTimeslots(toSave);
 		
 		List<Timeslot> dennisTimeslots = new ArrayList<>();
 		dennisTimeslots.add(afsUebung);
 		dennisTimeslots.add(afsVorlesung);
 		
+		List<Timeslot> chandlerTimeslots = new ArrayList<>();
+		chandlerTimeslots.add(afsVorlesung);
+		chandlerTimeslots.add(afsUebung2);
+		
 		dennis.setTimeslots(dennisTimeslots);
+		chandler.setTimeslots(chandlerTimeslots);
 		
-		
-		userRepository.save(krechel);
-		userRepository.save(weitz);
-		// SAVE Lecturers first so attendees can reference them
-		
-		userRepository.save(dennis);
-		userRepository.save(chandler);
+		List<User> usersToSave = new ArrayList<>();
+		usersToSave.add(dennis);
+		usersToSave.add(chandler);
+		userRepository.saveAll(usersToSave);
 		// TODO: Sinnvoll befüllen nach Model refactoring
 		
 		log.info("Done saving timeTable...");
