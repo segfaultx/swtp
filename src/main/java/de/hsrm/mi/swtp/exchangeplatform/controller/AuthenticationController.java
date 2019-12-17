35f5db56ca85e58cd10a52904a194ce4558f3d47
Amatus Muscheites <amatus.muscheites@gmail.com>
2019-12-17 15:01:20 +0100

    added openapi annotations to controllers, so client can use them

--- src/main/java/de/hsrm/mi/swtp/exchangeplatform/controller/AuthenticationController.java
+++ src/main/java/de/hsrm/mi/swtp/exchangeplatform/controller/AuthenticationController.java
@@ -8,10 +8,12 @@ import de.hsrm.mi.swtp.exchangeplatform.model.data.User;
 import de.hsrm.mi.swtp.exchangeplatform.service.authentication.AuthenticationService;
 import de.hsrm.mi.swtp.exchangeplatform.service.authentication.JWTTokenUtils;
 import de.hsrm.mi.swtp.exchangeplatform.service.rest.UserService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -32,13 +34,15 @@ public class AuthenticationController {
 	JWTTokenUtils jwtTokenUtil;
 	
 	@PostMapping("/login")
+	@ApiOperation(value = "login to application", nickname = "login")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "successfully logged in"),
+							@ApiResponse(code = 400, message = "malformed login request") })
 	public ResponseEntity<?> login(@RequestBody LoginRequestBody authenticationRequest) throws Exception {
 		authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
 		
 		if(!authenticationService.isLoginValid(authenticationRequest)) return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
 		
-		User user = userService.getByUsername(authenticationRequest.getUsername())
-				.orElseThrow(NotFoundException::new);
+		User user = userService.getByUsername(authenticationRequest.getUsername()).orElseThrow(NotFoundException::new);
 		
 		final String token = jwtTokenUtil.generateToken(user);
 		JWTResponse response = new JWTResponse(token);
@@ -46,6 +50,9 @@ public class AuthenticationController {
 	}
 	
 	@GetMapping("/whoami")
+	@ApiOperation(value = "whoami", nickname = "whoami")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "successfully fetched authentitacion info"),
+							@ApiResponse(code = 400, message = "malformed authinfo fetch request") })
 	public ResponseEntity<WhoAmI> getUser(@RequestHeader("Authorization") String token) throws Exception {
 		if(!jwtTokenUtil.isValidToken(token)) return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
 		
@@ -55,17 +62,17 @@ public class AuthenticationController {
 		WhoAmI whoAmI = userService.getWhoAmI(found);
 		return ResponseEntity.ok(whoAmI);
 	}
-
-
+	
+	
 	private void authenticate(String username, String password) throws Exception {
 		try {
 			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
-		} catch (DisabledException e) {
+		} catch(DisabledException e) {
 			throw new Exception("USER_DISABLED", e);
-		} catch (BadCredentialsException e) {
+		} catch(BadCredentialsException e) {
 			throw new Exception("INVALID_CREDENTIALS", e);
 		}
 	}
 	
-
+	
 }
