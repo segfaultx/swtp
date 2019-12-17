f20f86410c4a6ad983945ea774f08f2de91eb930
Amatus Muscheites <amatus.muscheites@gmail.com>
2019-12-17 17:19:52 +0100

    fixed tradeoffer backreference

--- src/main/java/de/hsrm/mi/swtp/exchangeplatform/model/data/TradeOffer.java
+++ src/main/java/de/hsrm/mi/swtp/exchangeplatform/model/data/TradeOffer.java
@@ -1,5 +1,6 @@
 package de.hsrm.mi.swtp.exchangeplatform.model.data;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AccessLevel;
 import lombok.Data;
@@ -22,6 +23,7 @@ public class TradeOffer implements Model {
 	
 	@JsonProperty("offerer")
 	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JsonBackReference
 	User offerer;
 	
 	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
