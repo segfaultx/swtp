933562e3fb532e984736361668c65dc50c3bb53c
Dennis Schad <dschad@comwrap.com>
2019-12-17 10:47:15 +0100

    Fixing things after merge

--- src/main/java/de/hsrm/mi/swtp/exchangeplatform/controller/TimeTableController.java
+++ src/main/java/de/hsrm/mi/swtp/exchangeplatform/controller/TimeTableController.java
@@ -1,8 +1,9 @@
 package de.hsrm.mi.swtp.exchangeplatform.controller;
 
+import de.hsrm.mi.swtp.exchangeplatform.exceptions.notfound.NotFoundException;
 import de.hsrm.mi.swtp.exchangeplatform.model.data.TimeTable;
-import de.hsrm.mi.swtp.exchangeplatform.service.rest.StudentService;
-import io.swagger.annotations.*;
+import de.hsrm.mi.swtp.exchangeplatform.service.rest.TimeTableService;
+import de.hsrm.mi.swtp.exchangeplatform.service.rest.UserService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -23,7 +24,8 @@ import java.time.LocalDate;
 @RestController
 public class TimeTableController {
 	
-	StudentService studentService;
+	UserService userService;
+	TimeTableService timeTableService;
 	
 	@GetMapping
 	public ResponseEntity<List<TimeTable>> getAll() {
---
+++ src/main/java/de/hsrm/mi/swtp/exchangeplatform/service/filter/TradeFilter/CapacityFilter.java
@@ -0,0 +1,13 @@
+package de.hsrm.mi.swtp.exchangeplatform.service.filter.TradeFilter;
+
+import de.hsrm.mi.swtp.exchangeplatform.model.data.TradeOffer;
+import de.hsrm.mi.swtp.exchangeplatform.service.filter.Filter;
+
+import java.util.List;
+
+public class CapacityFilter implements Filter {
+	@Override
+	public List<TradeOffer> filter(List<TradeOffer> offers) {
+		return null;
+	}
+}
