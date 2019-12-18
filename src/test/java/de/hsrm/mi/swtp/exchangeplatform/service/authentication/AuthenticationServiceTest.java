d83d347f1706119881dc645fa060e2df6ca4f65f
Dennis Schad <dschad@comwrap.com>
2019-12-18 12:33:02 +0100

    Added AuthenticationService Test

--- src/test/java/de/hsrm/mi/swtp/exchangeplatform/service/authentication/AuthenticationServiceTest.java
+++ src/test/java/de/hsrm/mi/swtp/exchangeplatform/service/authentication/AuthenticationServiceTest.java
@@ -1,42 +1,56 @@
 package de.hsrm.mi.swtp.exchangeplatform.service.authentication;
 
-import de.hsrm.mi.swtp.exchangeplatform.configuration.authentication.SecurityConfig;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.security.test.context.support.WithSecurityContext;
-import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
 public class AuthenticationServiceTest {
 
 	@Autowired
 	AuthenticationService authenticationService;
 	
 	@Test
-	@WithMockUser(username = "test", password = "hallo")
-	@WithUserDetails
 	public void isLoginValidTestValidCredentials() {
+
+		User.UserBuilder builder;
+		String password = "hallo";
+
+		builder = User.withUsername("test")
+					  .password(new BCryptPasswordEncoder().encode(password))
+					  .roles("ADMIN");
+		
+		UserDetails userDetails = builder.build();
+
+		boolean actual = authenticationService.isLoginValid(password, userDetails);
+
+		assertTrue(actual);
+	}
+	
+	@Test
+	public void isLoginValidTestInvalidCredentials() {
 		
 		User.UserBuilder builder;
 		String password = "hallo";
 		
-		builder = User.withUsername("test").password(new BCryptPasswordEncoder().encode(password));
+		builder = User.withUsername("test")
+					  .password(new BCryptPasswordEncoder().encode("password"))
+					  .roles("ADMIN");
 		
 		UserDetails userDetails = builder.build();
 		
 		boolean actual = authenticationService.isLoginValid(password, userDetails);
 		
-		assertTrue(actual);
+		assertFalse(actual);
 	}
 
 }
