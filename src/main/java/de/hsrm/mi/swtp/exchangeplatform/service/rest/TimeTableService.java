93cfdd03e9233e035fdc7c8fb6a82021776578f6
Amatus Muscheites <amatus.muscheites@gmail.com>
2019-12-18 10:18:27 +0100

    removed unused imports

--- src/main/java/de/hsrm/mi/swtp/exchangeplatform/service/rest/TimeTableService.java
+++ src/main/java/de/hsrm/mi/swtp/exchangeplatform/service/rest/TimeTableService.java
@@ -1,9 +1,5 @@
 package de.hsrm.mi.swtp.exchangeplatform.service.rest;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import de.hsrm.mi.swtp.exchangeplatform.exceptions.notfound.NotFoundException;
 import de.hsrm.mi.swtp.exchangeplatform.model.data.TimeTable;
 import de.hsrm.mi.swtp.exchangeplatform.repository.TimeTableRepository;
@@ -11,14 +7,10 @@ import de.hsrm.mi.swtp.exchangeplatform.repository.UserRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.IOException;
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
