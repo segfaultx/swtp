09708cdb7cff2969e169970648d240146b67e92a
Amatus Muscheites <amatus.muscheites@gmail.com>
2020-01-14 08:15:25 +0100

    added whoami test

--- src/test/java/de/hsrm/mi/swtp/exchangeplatform/controller/AuthenticationRestControllerTest.java
+++ src/test/java/de/hsrm/mi/swtp/exchangeplatform/controller/AuthenticationRestControllerTest.java
@@ -4,6 +4,8 @@ package de.hsrm.mi.swtp.exchangeplatform.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.jsonpath.JsonPath;
 import de.hsrm.mi.swtp.exchangeplatform.model.authentication.LoginRequestBody;
+import de.hsrm.mi.swtp.exchangeplatform.model.authentication.WhoAmI;
+import de.hsrm.mi.swtp.exchangeplatform.model.data.enums.Roles;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +15,9 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.util.AssertionErrors.assertEquals;
 import static org.springframework.test.util.AssertionErrors.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -27,13 +31,8 @@ public class AuthenticationRestControllerTest {
 	
 	@Test
 	void testLogin() throws Exception{
-		LoginRequestBody json = new LoginRequestBody();
-		json.setPassword("wweit001");
-		json.setUsername("wweit001");
-		var result = mockMvc.perform(post("/api/v1/auth/login")
-											 .contentType(MediaType.APPLICATION_JSON).content(new ObjectMapper().writeValueAsString(json)))
-							.andExpect(status().isOk()).andReturn();
-		assertNotNull("JWT Null",result.getResponse().getContentAsString());
+		var response = getLoginToken();
+		assertNotNull("JWT Null",response);
 	}
 	
 	@Test
@@ -49,18 +48,7 @@ public class AuthenticationRestControllerTest {
 	
 	@Test
 	void testLogout() throws Exception {
-		LoginRequestBody json = new LoginRequestBody();
-		json.setUsername("wweit001");
-		json.setPassword("wweit001");
-		var result = mockMvc.perform(post("/api/v1/auth/login")
-									.contentType(MediaType.APPLICATION_JSON)
-									.content(new ObjectMapper().writeValueAsString(json)))
-				.andExpect(status().isOk())
-							.andReturn()
-							.getResponse()
-							.getContentAsString();
-		var token = JsonPath.read(result, "$.tokenResponse.token");
-		
+		var token = getLoginToken();
 		mockMvc.perform(post("/api/v1/auth/logout")
 					   .header("Authorization","Bearer "+ token))
 			   .andExpect(status().isOk());
@@ -68,6 +56,26 @@ public class AuthenticationRestControllerTest {
 	
 	//TODO: wait for exception handling
 	void testFailedLogout() throws Exception {
+		var token = getLoginToken();
+		var failToken = token.replace("a", "e");
+		mockMvc.perform(post("/ap1/v1/auth/logout")
+									.header("Authorization","Bearer " + failToken))
+				.andExpect(status().isBadRequest());
+	}
+	
+	@Test
+	void testWhoAmI() throws Exception {
+		var token = getLoginToken();
+		var response = mockMvc.perform(get("/api/v1/auth/whoami")
+											   .header("Authorization", "Bearer " + token))
+							  .andExpect(status().isOk())
+			   .andReturn();
+		WhoAmI out = new ObjectMapper().readValue(response.getResponse().getContentAsString(), WhoAmI.class);
+		assertEquals("WhoAmI equals USERNAME", out.getUsername(), "wweit001");
+		assertEquals("WhoAmI equals ROLE", out.getRole(), Roles.ADMIN);
+	}
+	
+	private String getLoginToken() throws Exception {
 		LoginRequestBody json = new LoginRequestBody();
 		json.setUsername("wweit001");
 		json.setPassword("wweit001");
@@ -78,10 +86,6 @@ public class AuthenticationRestControllerTest {
 							.andReturn()
 							.getResponse()
 							.getContentAsString();
-		var token = JsonPath.read(result, "$.tokenResponse.token");
-		var failToken = ((String) token).replace("a", "e");
-		mockMvc.perform(post("/ap1/v1/auth/logout")
-									.header("Authorization","Bearer " + failToken))
-				.andExpect(status().isBadRequest());
+		return JsonPath.read(result, "$.tokenResponse.token");
 	}
 }
