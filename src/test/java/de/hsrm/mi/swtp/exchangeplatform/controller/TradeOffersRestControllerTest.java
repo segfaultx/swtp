37887d487fd1d78b90cf13154698ffc35b513f06
Amatus Muscheites <amatus.muscheites@gmail.com>
2020-01-15 08:48:19 +0100

    added create and delete tradeoffer test

--- src/test/java/de/hsrm/mi/swtp/exchangeplatform/controller/TradeOffersRestControllerTest.java
+++ src/test/java/de/hsrm/mi/swtp/exchangeplatform/controller/TradeOffersRestControllerTest.java
@@ -3,7 +3,6 @@ package de.hsrm.mi.swtp.exchangeplatform.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.jsonpath.JsonPath;
 import de.hsrm.mi.swtp.exchangeplatform.model.authentication.LoginRequestBody;
-import de.hsrm.mi.swtp.exchangeplatform.model.data.TradeOffer;
 import de.hsrm.mi.swtp.exchangeplatform.model.rest.TradeRequest;
 import de.hsrm.mi.swtp.exchangeplatform.repository.TradeOfferRepository;
 import org.junit.jupiter.api.Test;
@@ -15,9 +14,11 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Map;
+
 import static org.springframework.test.util.AssertionErrors.assertEquals;
 import static org.springframework.test.util.AssertionErrors.assertNotNull;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
@@ -50,6 +51,37 @@ public class TradeOffersRestControllerTest {
 		assertEquals("Tradeoffer offered id",11L, persistedTradeoffer.getOffer().getId());
 		assertEquals("Tradeoffer seek id",23L, persistedTradeoffer.getSeek().getId());
 		assertEquals("Offerer ID",8L, persistedTradeoffer.getOfferer().getId());
+		tradeOfferRepository.delete(persistedTradeoffer);
+	}
+	
+	@Test
+	void createAndDeleteTradeOffer() throws Exception {
+		var token = getLoginToken();
+		TradeRequest json = new TradeRequest();
+		json.setOfferedByStudentMatriculationNumber(8);
+		json.setOfferedTimeslotId(11);
+		json.setWantedTimeslotId(23);
+		var result = mockMvc.perform(post("/api/v1/trades/create")
+											 .contentType(MediaType.APPLICATION_JSON)
+											 .content(new ObjectMapper().writeValueAsString(json))
+											 .header("Authorization", "Bearer " + token))
+							.andExpect(status().isOk())
+							.andReturn();
+		var jsonString = result.getResponse().getContentAsString();
+		mockMvc.perform(delete("/api/v1/trades/8/23")
+								.header("Authorization", "Bearer " + token))
+			   .andExpect(status().isOk());
+	}
+	
+	@Test
+	void testGetTradeOffersForModule() throws Exception {
+		var token = getLoginToken();
+		var result = mockMvc.perform(get("/api/v1/trades/23")
+									.header("Authorization", "Bearer " + token)).andExpect(status().isOk())
+				.andReturn().getResponse().getContentAsString();
+		var deserializedResult = new ObjectMapper().readValue(result, Map.class);
+		assertNotNull("Tradeoffers Map null", deserializedResult);
+		
 	}
 	
 	private String getLoginToken() throws Exception {
