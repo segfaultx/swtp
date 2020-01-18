f02b3364c7df257ba7d7cebeef6fcb8c5037f415
Amatus Muscheites <amatus.muscheites@gmail.com>
2020-01-18 16:56:52 +0100

    added query to modulerepository to lookup all modules by PO

--- src/main/java/de/hsrm/mi/swtp/exchangeplatform/repository/ModuleRepository.java
+++ src/main/java/de/hsrm/mi/swtp/exchangeplatform/repository/ModuleRepository.java
@@ -1,8 +1,13 @@
 package de.hsrm.mi.swtp.exchangeplatform.repository;
 
 import de.hsrm.mi.swtp.exchangeplatform.model.data.Module;
+import de.hsrm.mi.swtp.exchangeplatform.model.data.PO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface ModuleRepository extends JpaRepository<Module, Long> {}
+public interface ModuleRepository extends JpaRepository<Module, Long> {
+	List<Module> findAllByPo(PO po);
+}
