66ad6b50b326beb7a6a3b8c6efe0867f3c5cc049
Mona Porcher <mona.porcher@gmail.com>
2020-01-18 20:08:15 +0100

    added Method to test for collisions of own timetable and additional module

--- src/main/java/de/hsrm/mi/swtp/exchangeplatform/controller/TimeTableController.java
+++ src/main/java/de/hsrm/mi/swtp/exchangeplatform/controller/TimeTableController.java
@@ -1,6 +1,8 @@
 package de.hsrm.mi.swtp.exchangeplatform.controller;
 
+import de.hsrm.mi.swtp.exchangeplatform.exceptions.notfound.NotFoundException;
 import de.hsrm.mi.swtp.exchangeplatform.model.data.TimeTable;
+import de.hsrm.mi.swtp.exchangeplatform.model.data.Timeslot;
 import de.hsrm.mi.swtp.exchangeplatform.service.rest.TimeTableService;
 import de.hsrm.mi.swtp.exchangeplatform.service.rest.UserService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -11,6 +13,7 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import org.checkerframework.checker.units.qual.A;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -19,6 +22,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.List;
 
 @Slf4j
@@ -51,4 +55,38 @@ public class TimeTableController {
 		
 		return ResponseEntity.ok(timeTable);
 	}
+	
+/*	@GetMapping("/modulesforstudent/{studentId}")
+	@Operation(description = "get potential modules for student", operationId = "getModulesForStudent")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "sucessfully fetched modules for student"),
+			@ApiResponse(responseCode = "403", description = "unauthorized fetch attempt"),
+			@ApiResponse(responseCode = "400", description = "malformed ID"),
+			@ApiResponse(responseCode = "404", description = "unknown student id")})
+	@PreAuthorize("hasRole('MEMBER')")
+	public ResponseEntity<List<Timeslot>> getTimeslotsForStudent(@PathVariable("studentId") Long studentId,
+																 Principal principal) throws NotFoundException {
+		log.info(String.format("GET REQUEST: getModulesForStudent, by user: %s, for studentid %d",
+							   principal.getName(), studentId));
+		var potentialModules = moduleService.lookUpAvailableModulesForStudent(principal.getName());
+		return new ResponseEntity<>(potentialModules, HttpStatus.OK);
+	}*/
+	
+	
+	@GetMapping("/suggestedTimetable/{timeslotid}")
+	@Operation(description = "get suggested timetable per Module for student", operationId = "getSuggestedTimetable")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "successfully fetched suggested timetable for student"),
+			@ApiResponse(responseCode = "403", description = "unauthorized fetch attempt"),
+			@ApiResponse(responseCode = "404", description = "unkown timeslot id"),
+			@ApiResponse(responseCode = "400", description = "bad request")})
+	@PreAuthorize("hasRole('MEMBER')")
+	public ResponseEntity<TimeTable> getSuggestedTimetableForStudent(@PathVariable("timeslotid") Long timeslotID,
+																	 Principal principal) throws NotFoundException {
+		log.info(String.format("GET REQUEST: getSuggestedTimetableForStudent, by user: %s, for timeslotid: %d", principal.getName(), timeslotID));
+		
+		return null;
+	}
+	
+	
 }
--- src/main/java/de/hsrm/mi/swtp/exchangeplatform/service/rest/TimeTableService.java
+++ src/main/java/de/hsrm/mi/swtp/exchangeplatform/service/rest/TimeTableService.java
@@ -2,14 +2,21 @@ package de.hsrm.mi.swtp.exchangeplatform.service.rest;
 
 import de.hsrm.mi.swtp.exchangeplatform.exceptions.notfound.NotFoundException;
 import de.hsrm.mi.swtp.exchangeplatform.model.data.TimeTable;
+import de.hsrm.mi.swtp.exchangeplatform.model.data.Timeslot;
+import de.hsrm.mi.swtp.exchangeplatform.model.data.enums.TypeOfTimeslots;
+import de.hsrm.mi.swtp.exchangeplatform.repository.ModuleRepository;
 import de.hsrm.mi.swtp.exchangeplatform.repository.TimeTableRepository;
+import de.hsrm.mi.swtp.exchangeplatform.repository.TimeslotRepository;
 import de.hsrm.mi.swtp.exchangeplatform.repository.UserRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -19,6 +26,7 @@ public class TimeTableService {
 	
 	TimeTableRepository repository;
 	UserRepository userRepository;
+	TimeslotRepository timeslotRepository;
 	
 	public List<TimeTable> getAll() {
 		return repository.findAll();
@@ -37,5 +45,40 @@ public class TimeTableService {
 	
 	public void save(TimeTable timeTable) {
 		repository.save(timeTable);
+	}
+	
+	public TimeTable getSuggestedTimetable(Long timeslotID, String username){
+		TimeTable tempTimetable = new TimeTable();
+		var user = userRepository.findByUsername(username).orElseThrow();
+		var timeslot = timeslotRepository.findById(timeslotID).orElseThrow();
+		var module = timeslot.getModule();
+		var allTimeslotsOfModule = module.getTimeslots();
+		tempTimetable.setTimeslots(user.getTimeslots());
+		List<Timeslot> potentialTimeslots = new ArrayList<>();
+		allTimeslotsOfModule.forEach(ts -> { if (hasTimeslotSpace(ts)) potentialTimeslots.add(ts);});
+		if(potentialTimeslots.size() < 2) return null;
+		if(potentialTimeslots.stream().noneMatch(item -> item.getTimeSlotType() == TypeOfTimeslots.VORLESUNG)) return null;
+		//TODO: überprüfen welche Timeslots collision haben, wenn alle -> Micha fragen wie man damit umgeht?, ansonsten ersten ohne Collision in timetable einbauen
+		return null;
+	}
+	
+	public boolean hasCollisions(Timeslot timeslot, TimeTable timeTable){
+		for(Timeslot ts : timeTable.getTimeslots()) {
+			if (ts.getDay() == timeslot.getDay()){
+				if(hoursAreColliding(ts.getTimeEnd(), ts.getTimeStart(), timeslot.getTimeEnd(), timeslot.getTimeStart())) return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean hoursAreColliding(LocalTime aTimeEnd, LocalTime aTimeStart, LocalTime bTimeEnd, LocalTime bTimeStart){
+		if(aTimeStart.equals(bTimeStart)  || aTimeEnd.equals(bTimeEnd)) return true;
+		if((aTimeStart.isBefore(bTimeEnd) && aTimeStart.isAfter(bTimeStart)) || (bTimeStart.isBefore(aTimeEnd) && bTimeStart.isAfter(aTimeStart))) return true;
+		if((aTimeStart.isBefore(bTimeStart) && aTimeEnd.isBefore(bTimeEnd)) || (bTimeStart.isBefore(aTimeStart) && bTimeEnd.isBefore(aTimeEnd))) return true;
+		return false;
+	}
+	
+	public boolean hasTimeslotSpace(Timeslot timeslot){
+		return (timeslot.getCapacity() - timeslot.getAttendees().size()) > 0;
 	}
 }
