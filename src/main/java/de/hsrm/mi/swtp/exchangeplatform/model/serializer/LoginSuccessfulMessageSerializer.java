9beb2f183b3971d6497981bc15f70b92a43dbcbf
Henock Arega <harega@seibert-media.net>
2020-01-25 00:50:42 +0100

    Fixed value of LoginSuccessfulMessage.

--- src/main/java/de/hsrm/mi/swtp/exchangeplatform/model/serializer/LoginSuccessfulMessageSerializer.java
+++ src/main/java/de/hsrm/mi/swtp/exchangeplatform/model/serializer/LoginSuccessfulMessageSerializer.java
@@ -3,7 +3,6 @@ package de.hsrm.mi.swtp.exchangeplatform.model.serializer;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
-import de.hsrm.mi.swtp.exchangeplatform.messaging.message.ExchangeplatformStatusMessage;
 import de.hsrm.mi.swtp.exchangeplatform.messaging.message.LoginSuccessfulMessage;
 
 import java.io.IOException;
@@ -22,6 +21,10 @@ public class LoginSuccessfulMessageSerializer extends StdSerializer<LoginSuccess
 	public void serialize(LoginSuccessfulMessage value, JsonGenerator gen, SerializerProvider provider) throws IOException {
 		gen.writeStartObject();
 		gen.writeStringField("message", value.getMessage());
+		gen.writeFieldName("value");
+		gen.writeStartObject();
+		gen.writeStringField("timestamp", value.getTimestampString());
+		gen.writeEndObject();
 		gen.writeEndObject();
 	}
 	
