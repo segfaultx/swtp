1096e03610c14c6bcf6d03569a2a19f486f9b8a9
Mona Porcher <mona.porcher@gmail.com>
2020-01-25 16:44:04 +0100

    added po to students in initiator

--- src/main/java/de/hsrm/mi/swtp/exchangeplatform/configuration/initiator/DBInitiator.java
+++ src/main/java/de/hsrm/mi/swtp/exchangeplatform/configuration/initiator/DBInitiator.java
@@ -459,11 +459,13 @@ public class DBInitiator implements ApplicationRunner {
 		ArrayList<User> students_repo = new ArrayList<>();
 		for(User student : usersToSave) {
 			User student_repo = userRepository.getOne(student.getId());
+			student.setPo(po2017);
 			students_repo.add(student_repo);
 		}
 		po2017_repo.setStudents(students_repo);
 		
 		poRepository.save(po2017_repo);
+		userRepository.saveAll(usersToSave);
 		
 		log.info("Done saving timeTable...");
 	}
