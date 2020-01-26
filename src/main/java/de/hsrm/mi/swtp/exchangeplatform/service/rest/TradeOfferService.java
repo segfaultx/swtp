f52774f54d43d773af996909ca2351dc894f669e
Amatus Muscheites <amatus.muscheites@gmail.com>
2020-01-26 14:03:01 +0100

    updated tradeofferservice

--- src/main/java/de/hsrm/mi/swtp/exchangeplatform/service/rest/TradeOfferService.java
+++ src/main/java/de/hsrm/mi/swtp/exchangeplatform/service/rest/TradeOfferService.java
@@ -31,6 +31,7 @@ public class TradeOfferService implements RestService<TradeOffer, Long> {
 	UserRepository userRepository;
 	AdminTradeService adminTradeService;
 	TradeService tradeService;
+	FilterUtils filterUtils;
 
 	/**
 	 * Method to provide admins a forced trade / assignment of timeslot to a given student
@@ -143,12 +144,7 @@ public class TradeOfferService implements RestService<TradeOffer, Long> {
 									 !(tradeOffer.getOffer() == requestedTimeslot && tradeOffer.getSeek() == offeredTimeslot));
 		
 		// filter the list according to active filters
-		FilterUtils filterUtils = FilterUtils.getInstance();
-		try {
 			tradeOffers = filterUtils.getFilteredTradeOffers(tradeOffers);
-		} catch(NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
-			e.printStackTrace();
-		}
 		
 		// if no matching TradeOffer was found return null
 		if(tradeOffers.size() == 0) return null;
