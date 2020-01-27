f8fe276dd5afd185dc31a89e8d10c85d154634e8
Amatus Muscheites <amatus.muscheites@gmail.com>
2020-01-27 10:24:36 +0100

    updated tests to match new api requirements

--- src/test/java/de/hsrm/mi/swtp/exchangeplatform/controller/AdminRestControllerTest.java
+++ src/test/java/de/hsrm/mi/swtp/exchangeplatform/controller/AdminRestControllerTest.java
@@ -1,7 +1,9 @@
 package de.hsrm.mi.swtp.exchangeplatform.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import de.hsrm.mi.swtp.exchangeplatform.model.admin.settings.AdminSettings;
+import de.hsrm.mi.swtp.exchangeplatform.model.rest.AdminSettingsRequest;
 import de.hsrm.mi.swtp.exchangeplatform.repository.AdminSettingsRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.BeanUtils;
@@ -11,6 +13,9 @@ import org.springframework.security.test.context.support.WithMockUser;
 
 
 import javax.transaction.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 import static org.springframework.test.util.AssertionErrors.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -26,8 +31,10 @@ public class AdminRestControllerTest extends BaseRestTest {
 	@WithMockUser(roles = "ADMIN")
 	@Transactional
 	void testGetAdminsettings() throws Exception {
+		ObjectMapper serializer = new ObjectMapper();
+		serializer.registerModule(new JavaTimeModule());
 		var result = mockMvc.perform(get("/api/v1/admin/settings")).andExpect(status().isOk()).andReturn();
-		var deserialized_result = new ObjectMapper().readValue(result.getResponse().getContentAsString(), AdminSettings.class);
+		var deserialized_result = serializer.readValue(result.getResponse().getContentAsString(), AdminSettings.class);
 		var actual_setting = adminSettingsRepository.findById(1L).orElseThrow();
 		assertEquals("Adminsettings get equals", actual_setting, deserialized_result);
 	}
@@ -36,16 +43,19 @@ public class AdminRestControllerTest extends BaseRestTest {
 	@WithMockUser(roles = "ADMIN")
 	@Transactional
 	void testPostAdminsettings() throws Exception {
-		var actual_setting = adminSettingsRepository.findById(1L).orElseThrow();
-		AdminSettings newAdminsetting = new AdminSettings();
-		BeanUtils.copyProperties(actual_setting, newAdminsetting);
-		newAdminsetting.setTradesActive(false);
+		ObjectMapper serializer = new ObjectMapper();
+		serializer.registerModule(new JavaTimeModule());
+		AdminSettingsRequest request = new AdminSettingsRequest();
+		request.setActiveFilters(new ArrayList<>());
+		request.setDateEndTrades(LocalDateTime.now());
+		request.setDateStartTrades(LocalDateTime.now());
+		request.setTradesActive(false);
 		var result = mockMvc.perform(
-				post("/api/v1/admin/settings").contentType(MediaType.APPLICATION_JSON).content(new ObjectMapper().writeValueAsString(newAdminsetting)))
+				post("/api/v1/admin/settings").contentType(MediaType.APPLICATION_JSON).content(serializer.writeValueAsString(request)))
 							.andExpect(status().isOk())
 							.andReturn();
 		var changed_setting = adminSettingsRepository.findById(1L).orElseThrow();
-		var settings_response = new ObjectMapper().readValue(result.getResponse().getContentAsString(), AdminSettings.class);
+		var settings_response = serializer.readValue(result.getResponse().getContentAsString(), AdminSettings.class);
 		assertEquals("Adminsettings post equals", changed_setting, settings_response);
 	}
 	
