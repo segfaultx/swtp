24fb2b192d7e25dbdbbf1a4b222c331b0fd845e2
Amatus Muscheites <amatus.muscheites@gmail.com>
2020-01-28 11:09:44 +0100

    fixed modulesforstudent route

--- src/main/java/de/hsrm/mi/swtp/exchangeplatform/controller/ModuleRestController.java
+++ src/main/java/de/hsrm/mi/swtp/exchangeplatform/controller/ModuleRestController.java
@@ -2,6 +2,8 @@ package de.hsrm.mi.swtp.exchangeplatform.controller;
 
 import de.hsrm.mi.swtp.exchangeplatform.exceptions.UserIsAlreadyAttendeeException;
 import de.hsrm.mi.swtp.exchangeplatform.exceptions.notfound.NotFoundException;
+import de.hsrm.mi.swtp.exchangeplatform.model.data.enums.Roles;
+import de.hsrm.mi.swtp.exchangeplatform.model.data.enums.TypeOfUsers;
 import de.hsrm.mi.swtp.exchangeplatform.model.rest.ModuleRequestBody;
 import de.hsrm.mi.swtp.exchangeplatform.model.data.Module;
 import de.hsrm.mi.swtp.exchangeplatform.model.data.Timeslot;
@@ -124,13 +126,16 @@ public class ModuleRestController {
 			@ApiResponse(responseCode = "403", description = "unauthorized fetch attempt"),
 			@ApiResponse(responseCode = "400", description = "malformed ID"),
 			@ApiResponse(responseCode = "404", description = "unknown student id")})
-	@PreAuthorize("hasRole('MEMBER')")
+	@PreAuthorize("hasRole('MEMBER') or hasRole('ADMIN')")
 	public ResponseEntity<List<Timeslot>> getTimeslotsForStudent(@PathVariable("studentId") Long studentId,
 																 Principal principal) throws NotFoundException {
 		log.info(String.format("GET REQUEST: getModulesForStudent, by user: %s, for studentid %d",
 							   principal.getName(), studentId));
 		log.info(String.format("LOOKING UP USER WITH USERNAME: %s", principal.getName()));
 		var usr = userService.getByUsername(principal.getName()).orElseThrow(NotFoundException::new);
+		if (usr.getId().equals(studentId) && usr.getAuthenticationInformation().getRole() != Roles.ADMIN){
+			return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+		};
 		var potentialModules = moduleService.lookUpAvailableModulesForStudent(usr);
 		return new ResponseEntity<>(potentialModules, HttpStatus.OK);
 	}
