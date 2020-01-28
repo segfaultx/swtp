d5e2baf082ce69359d2bd9370cb7737f82650096
Dennis Schad <dennis.schad@student.hs-rm.de>
2020-01-28 19:05:34 +0100

    docs and unit test

--- src/main/java/de/hsrm/mi/swtp/exchangeplatform/service/rest/TradeService.java
+++ src/main/java/de/hsrm/mi/swtp/exchangeplatform/service/rest/TradeService.java
@@ -1,6 +1,5 @@
 package de.hsrm.mi.swtp.exchangeplatform.service.rest;
 
-import de.hsrm.mi.swtp.exchangeplatform.exceptions.notfound.NotFoundException;
 import de.hsrm.mi.swtp.exchangeplatform.model.data.Timeslot;
 import de.hsrm.mi.swtp.exchangeplatform.model.data.User;
 import org.springframework.stereotype.Service;
@@ -12,5 +11,5 @@ import org.springframework.stereotype.Service;
 @Service
 public interface TradeService {
 	
-	boolean doTrade(User student1, User student2, Timeslot timeslot1, Timeslot timeslot2) throws NotFoundException;
+	boolean doTrade(User student1, User student2, Timeslot timeslot1, Timeslot timeslot2);
 }
